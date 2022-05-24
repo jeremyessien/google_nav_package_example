a13df55c6a8ccf349f9075c5976d168b674365d9
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-05-24 13:18:30 +0100

    revert - fixed parent widget in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:google_nav_package_example/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,18 +7,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: GNav(tabs: [
-        GButton(icon: Icons.home, text: 'Home',),
-        GButton(icon: Icons.search, text: 'Search',),
-        GButton(icon: Icons.favorite_border, text: 'Likes',),
-        GButton(icon: Icons.settings, text: 'Setting',),
-
-
-      ],),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomNavBar(),
     );
   }
 }
