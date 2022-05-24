8363547f3348bd3d8470d6ced08893c88dd08ed1
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-05-24 13:46:39 +0100

    perf

--- lib/bottom_nav_bar.dart
+++ lib/bottom_nav_bar.dart
@@ -3,7 +3,6 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({Key? key}) : super(key: key);
-
   @override
   _BottomNavBarState createState() => _BottomNavBarState();
 }
@@ -11,13 +10,37 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: GNav(tabs: [
-        GButton(icon: Icons.home, text: 'Home',),
-      GButton(icon: Icons.search, text: 'Search',),
-      GButton(icon: Icons.favorite_border, text: 'Likes',),
-      GButton(icon: Icons.settings, text: 'Setting',),
-  ]
+    return  Scaffold(
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+          child: GNav(
+            backgroundColor: Colors.black,
+              color: Colors.white,
+              padding: EdgeInsets.all(16),
+              activeColor: Colors.white,
+              tabBackgroundColor: Colors.grey.shade800,
+              gap: 10,
+              tabs: const [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.search,
+              text: 'Search',
+            ),
+            GButton(
+              icon: Icons.favorite_border,
+              text: 'Likes',
+            ),
+            GButton(
+              icon: Icons.settings,
+              text: 'Setting',
+            ),
+          ]),
+        ),
       ),
     );
   }
